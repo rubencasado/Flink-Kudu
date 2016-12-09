472ac3213be80e0c0622edd840c879967b437cef
AlvaroVadillo <alvaro.vadillo.av@gmail.com>
2016-12-09 12:39:14 +0100

    Generado primera version de RowSerializable y modificado el main

--- src/main/java/es/accenture/flink/Sources/main_source.java
+++ src/main/java/es/accenture/flink/Sources/main_source.java
@@ -1,17 +1,22 @@
 package es.accenture.flink.Sources;
 
+import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeutils.TypeSerializer;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.api.java.operators.DataSource;
-import org.apache.flink.api.java.tuple.Tuple2;
+
+import org.apache.flink.api.java.io.TypeSerializerOutputFormat;
 import org.apache.flink.api.table.Row;
+import org.apache.flink.api.table.typeutils.RowTypeInfo;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.core.io.InputSplit;
+
 import org.apache.kudu.client.RowResult;
 import org.apache.kudu.client.RowResultIterator;
 
-import java.io.IOException;
+
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,9 +30,9 @@ public class main_source {
 
     public static void main (String[] args) throws Exception {
 
-        List<Row> rows = new ArrayList<>();
+        List<RowSerializable> rows = new ArrayList<>();
         RowResult rowRes;
-        Row row;
+        RowSerializable row;
 
         KuduInputFormat prueba = new KuduInputFormat();
         KuduInputSplit a = null;
@@ -35,42 +40,85 @@ public class main_source {
         System.out.println("Salido de configure");
         prueba.open(a);
         System.out.println("Salido de Open");
-        RowResultIterator results=prueba.getResults();
-        System.out.println(rows.toString());
-        try {
-            rowRes=results.next();
-            row=prueba.RowResultToRow(rowRes);
-        } catch (Exception e){
-            rowRes=null;
-            row=prueba.RowResultToRow(rowRes);
-            System.out.println("TABLA VACIA");
-        }
-        while(results.hasNext()) {
-            rows.add(row);
-            row=prueba.nextRecord(row);
-        }
-        rows.add(row);
 
-        for (Row r : rows){
+
+        for (RowSerializable r : prueba.getRows()){
             System.out.println(r);
         }
 
         System.out.println("Generando dataset");
-        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
-        DataSet<Row> data = env
-                .createInput(prueba);
 
-        data.print();
+
+
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        DataSet<RowSerializable> data = env.createInput(prueba, new TypeInformation<RowSerializable>() {
+            @Override
+            public boolean isBasicType() {
+                return false;
+            }
+
+            @Override
+            public boolean isTupleType() {
+                return false;
+            }
+
+            @Override
+            public int getArity() {
+                return 0;
+            }
+
+            @Override
+            public int getTotalFields() {
+                return 0;
+            }
+
+            @Override
+            public Class<RowSerializable> getTypeClass() {
+                return null;
+            }
+
+            @Override
+            public boolean isKeyType() {
+                return false;
+            }
+
+            @Override
+            public TypeSerializer<RowSerializable> createSerializer(ExecutionConfig executionConfig) {
+                return null;
+            }
+
+            @Override
+            public String toString() {
+                return null;
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                return false;
+            }
+
+            @Override
+            public int hashCode() {
+                return 0;
+            }
+
+            @Override
+            public boolean canEqual(Object o) {
+                return false;
+            }
+        });
+
+
 
 
         System.out.println("dataset creado");
-
-        DataSet<Row> data2 = data
-                .map(new MapFunction<Row, Row>() {
+/*
+        DataSet<RowSerializable> data2 = data
+                .map(new MapFunction<RowSerializable, RowSerializable>()  {
 
                     @Override
-                    public Row map(Row row) throws Exception {
-                        Row row2 = row;
+                    public RowSerializable map(RowSerializable row) throws Exception {
+                        RowSerializable row2 = row;
                         System.out.println("aaaaaaaaa");
                         String value = row.productElement(1).toString();
                         if (value.startsWith("value")) {
@@ -80,8 +128,10 @@ public class main_source {
                         return row;
                     }
                 });
-
+        System.out.println("Fuera map");
+*/
     }
+
 
 
 
