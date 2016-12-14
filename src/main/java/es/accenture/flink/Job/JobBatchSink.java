f03c6419a706e00ddb1beeb62f7999e7fcda4c41
Sergiy <sergiyshvayka7@gmail.com>
2016-12-14 13:46:21 +0100

    Modified sink classes names, refactored comments and methods of Utils and created Streaming Sink

--- src/main/java/es/accenture/flink/Job/JobBatchSink.java
+++ src/main/java/es/accenture/flink/Job/JobBatchSink.java
@@ -1,6 +1,6 @@
 package es.accenture.flink.Job;
 
-import es.accenture.flink.Sink.KuduSinkFunction;
+import es.accenture.flink.Sink.KuduOutputFormat;
 
 import es.accenture.flink.Utils.RowSerializable;
 import org.apache.flink.api.common.functions.MapFunction;
@@ -10,7 +10,7 @@ import org.apache.flink.api.java.ExecutionEnvironment;
 /**
  * Created by dani on 9/12/16.
  */
-public class JobSink {
+public class JobBatchSink {
 
     public static final String KUDU_MASTER = System.getProperty("kuduMaster", "localhost");
     public static final String TABLE_NAME = System.getProperty("tableName", "sample");
@@ -20,11 +20,11 @@ public class JobSink {
         String [] columnNames = new String[3];
         columnNames[0] = "key";
         columnNames[1] = "value";
-        columnNames[2] = "descripcion";
+        columnNames[2] = "description";
 
         final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-        DataSet<String> input = env.fromElements("fila100 value100 descripcion1000");
+        DataSet<String> input = env.fromElements("fila100 value100 description1000");
 
         DataSet<RowSerializable> out = input.map(new MapFunction<String, RowSerializable>() {
             @Override
@@ -39,7 +39,7 @@ public class JobSink {
             }
         });
 
-        out.output(new KuduSinkFunction(KUDU_MASTER, TABLE_NAME, columnNames, "CREATE"));
+        out.output(new KuduOutputFormat(KUDU_MASTER, TABLE_NAME, columnNames, "CREATE"));
         env.execute();
 
     }
