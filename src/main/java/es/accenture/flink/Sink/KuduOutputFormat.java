e0a88439cdc98548efffe39bfb21a098de3c0c40
Sergiy <sergiyshvayka7@gmail.com>
2017-01-03 13:01:59 +0100

    Added Exception handling for Batch classes

--- src/main/java/es/accenture/flink/Sink/KuduOutputFormat.java
+++ src/main/java/es/accenture/flink/Sink/KuduOutputFormat.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 public class KuduOutputFormat extends RichOutputFormat<RowSerializable> {
 
     private String host, tableName, tableMode;
-    private String [] fieldsNames;
+    private String[] fieldsNames;
     private transient Utils utils;
 
     //Kudu variables
@@ -26,67 +26,66 @@ public class KuduOutputFormat extends RichOutputFormat<RowSerializable> {
     /**
      * Builder to use when you want to create a new table
      *
-     * @param host          Kudu host
-     * @param tableName     Kudu table name
-     * @param fieldsNames   List of column names in the table to be created
-     * @param tableMode     Way to operate with table (CREATE,APPEND,OVERRIDE)
-     * @throws KuduException
+     * @param host        Kudu host
+     * @param tableName   Kudu table name
+     * @param fieldsNames List of column names in the table to be created
+     * @param tableMode   Way to operate with table (CREATE, APPEND, OVERRIDE)
+     * @throws IllegalArgumentException when wrong params
      */
-    public KuduOutputFormat(String host, String tableName, String [] fieldsNames, String tableMode) throws Exception {
-        if (tableMode == null || tableMode.isEmpty()){
-            throw new Exception("ERROR: Param \"tableMode\" not valid (null or empty)");
+    public KuduOutputFormat(String host, String tableName, String[] fieldsNames, String tableMode) throws IllegalArgumentException {
+        if (tableMode == null || tableMode.isEmpty()) {
+            throw new IllegalArgumentException("ERROR: Param \"tableMode\" not valid (null or empty)");
 
-        } else if (!(tableMode.equals("CREATE") || tableMode.equals("APPEND") || tableMode.equals("OVERRIDE"))){
-            throw new Exception("ERROR: Param \"tableMode\" not valid (must be CREATE, APPEND or OVERRIDE)");
+        } else if (!(tableMode.equals("CREATE") || tableMode.equals("APPEND") || tableMode.equals("OVERRIDE"))) {
+            throw new IllegalArgumentException("ERROR: Param \"tableMode\" not valid (must be CREATE, APPEND or OVERRIDE)");
 
-        } else if (tableMode.equals("CREATE")){
-            if(fieldsNames == null || fieldsNames.length == 0){
-                throw new Exception("ERROR: Missing param \"fieldNames\". Can't create a table without column names");
-            }
-        } else if (host == null || host.isEmpty()){
-            throw new Exception("ERROR: Param \"host\" not valid (null or empty)");
+        } else if (tableMode.equals("CREATE")) {
+            if (fieldsNames == null || fieldsNames.length == 0)
+                throw new IllegalArgumentException("ERROR: Missing param \"fieldNames\". Can't create a table without column names");
 
-        } else if (tableName == null || tableName.isEmpty()){
-            throw new Exception("ERROR: Param \"tableName\" not valid (null or empty)");
+        } else if (host == null || host.isEmpty()) {
+            throw new IllegalArgumentException("ERROR: Param \"host\" not valid (null or empty)");
 
-        } else {
-            this.host = host;
-            this.tableName = tableName;
-            this.fieldsNames = fieldsNames;
-            this.tableMode = tableMode;
+        } else if (tableName == null || tableName.isEmpty()) {
+            throw new IllegalArgumentException("ERROR: Param \"tableName\" not valid (null or empty)");
         }
+
+        this.host = host;
+        this.tableName = tableName;
+        this.fieldsNames = fieldsNames;
+        this.tableMode = tableMode;
     }
 
     /**
      * Builder to be used when using an existing table
      *
-     * @param host          Kudu host
-     * @param tableName     Kudu table name to be used
-     * @param tableMode     Way to operate with table (CREATE,APPEND,OVERRIDE)
-     * @throws KuduException
+     * @param host      Kudu host
+     * @param tableName Kudu table name to be used
+     * @param tableMode Way to operate with table (CREATE, APPEND, OVERRIDE)
+     * @throws IllegalArgumentException when wrong params
      */
-    public KuduOutputFormat(String host, String tableName, String tableMode) throws Exception {
-        if (tableMode == null || tableMode.isEmpty()){
-            throw new Exception("ERROR: Param \"tableMode\" not valid (null or empty)");
+    public KuduOutputFormat(String host, String tableName, String tableMode) throws IllegalArgumentException {
+        if (tableMode == null || tableMode.isEmpty()) {
+            throw new IllegalArgumentException("ERROR: Param \"tableMode\" not valid (null or empty)");
 
-        } else if (tableMode.equals("CREATE")){
-            throw new Exception("ERROR: Param \"tableMode\" can't be CREATE if missing \"fieldNames\". Use other builder for this mode");
+        } else if (tableMode.equals("CREATE")) {
+            throw new IllegalArgumentException("ERROR: Param \"tableMode\" can't be CREATE if missing \"fieldNames\". Use other builder for this mode");
 
-        } else if (!(tableMode.equals("APPEND") || tableMode.equals("OVERRIDE"))){
-            throw new Exception("ERROR: Param \"tableMode\" not valid (must be APPEND or OVERRIDE)");
+        } else if (!(tableMode.equals("APPEND") || tableMode.equals("OVERRIDE"))) {
+            throw new IllegalArgumentException("ERROR: Param \"tableMode\" not valid (must be APPEND or OVERRIDE)");
 
-        } else if (host == null || host.isEmpty()){
-            throw new Exception("ERROR: Param \"host\" not valid (null or empty)");
+        } else if (host == null || host.isEmpty()) {
+            throw new IllegalArgumentException("ERROR: Param \"host\" not valid (null or empty)");
 
-        } else if (tableName == null || tableName.isEmpty()){
-            throw new Exception("ERROR: Param \"tableName\" not valid (null or empty)");
-
-        } else {
-            this.host = host;
-            this.tableName = tableName;
-            this.tableMode = tableMode;
+        } else if (tableName == null || tableName.isEmpty()) {
+            throw new IllegalArgumentException("ERROR: Param \"tableName\" not valid (null or empty)");
         }
+
+        this.host = host;
+        this.tableName = tableName;
+        this.tableMode = tableMode;
     }
+
 
     @Override
     public void configure(Configuration configuration) {
@@ -110,31 +109,20 @@ public class KuduOutputFormat extends RichOutputFormat<RowSerializable> {
      * @throws IOException
      */
     @Override
-    public void writeRecord(RowSerializable row) throws KuduException {
+    public void writeRecord(RowSerializable row) throws IOException {
 
-        try{
-            this.utils = new Utils(host);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+        // Establish connection with Kudu
+        this.utils = new Utils(host);
 
-        //Look at the situation of the table (exist or not) depending of the mode, the table is created or opened
-        try {
-            this.table = utils.useTable(tableName, fieldsNames, row, tableMode);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Look at the situation of the table (exist or not). Depending of the mode, the table is created or opened
+        this.table = utils.useTable(tableName, fieldsNames, row, tableMode);
 
-        // Case APPEND (or OVERRIDE), with builder without column names , because otherwise it throws a NullPointerException
+        // Case APPEND (or OVERRIDE), with builder without column names, because otherwise it throws a NullPointerException
         if(fieldsNames == null){
             fieldsNames = utils.getNamesOfColumns(table);
         } else {
             // When column names provided, and table exists, must check if column names match
-            try {
-                utils.checkNamesOfColumns(utils.getNamesOfColumns(table), fieldsNames);
-            } catch (Exception e){
-                e.printStackTrace();
-            }
+            utils.checkNamesOfColumns(utils.getNamesOfColumns(table), fieldsNames);
         }
 
         // Make the insert into the table
